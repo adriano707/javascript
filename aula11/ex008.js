11da24eb2c55735d86e4af8c27684a1d924f552a
Adriano Santana <mano1die2mili@gmail.com>
2020-03-31 10:31:46 -0300

    projetos js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59.9
+console.log(`A velocidade de seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula12/ex011.js
@@ -0,0 +1,8 @@
+idade = 66
+if (idade < 16){
+    console.log('Naõ vota')
+} else if (idade < 18 || idade > 65) {
+    console.log('Voto opcional')
+} else if (idade >= 18) {
+    console.log('Voto obrigatório')
+}
